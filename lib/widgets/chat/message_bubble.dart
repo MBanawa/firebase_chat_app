8382342737a8383bebc50df6fa96b6ef54b8bdd7
MBanawa <mikebanawa25@gmail.com>
2020-12-17 04:08:40 +0800

    display user image

--- lib/widgets/chat/message_bubble.dart
+++ lib/widgets/chat/message_bubble.dart
@@ -1,59 +1,81 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class MessageBubble extends StatelessWidget {
   final String message;
   final String username;
+  final String userImage;
   final bool isMe;
   final Key key;
 
-  MessageBubble(this.message, this.username, this.isMe, {this.key});
+  MessageBubble(
+    this.message,
+    this.username,
+    this.userImage,
+    this.isMe, {
+    this.key,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
+    return Stack(
       children: [
-        Container(
-          decoration: BoxDecoration(
-            color: isMe ? Colors.grey[300] : Theme.of(context).accentColor,
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(16),
-              topRight: Radius.circular(16),
-              bottomLeft: !isMe ? Radius.circular(0) : Radius.circular(12),
-              bottomRight: isMe ? Radius.circular(0) : Radius.circular(12),
+        Row(
+          mainAxisAlignment:
+              isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: isMe ? Colors.grey[300] : Theme.of(context).accentColor,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(16),
+                  topRight: Radius.circular(16),
+                  bottomLeft: !isMe ? Radius.circular(0) : Radius.circular(12),
+                  bottomRight: isMe ? Radius.circular(0) : Radius.circular(12),
+                ),
+              ),
+              width: MediaQuery.of(context).size.width / 1.7,
+              padding:
+                  const EdgeInsets.symmetric(vertical: 15.0, horizontal: 16.0),
+              margin:
+                  const EdgeInsets.symmetric(vertical: 15.0, horizontal: 8.0),
+              child: Column(
+                crossAxisAlignment:
+                    isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    username,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: isMe
+                          ? Colors.black
+                          : Theme.of(context).accentTextTheme.headline6.color,
+                    ),
+                  ),
+                  Text(
+                    message,
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      color: isMe
+                          ? Colors.black
+                          : Theme.of(context).accentTextTheme.headline6.color,
+                    ),
+                    textAlign: isMe ? TextAlign.end : TextAlign.start,
+                  ),
+                ],
+              ),
             ),
-          ),
-          width: MediaQuery.of(context).size.width / 1.4,
-          padding: const EdgeInsets.symmetric(vertical: 15.0, horizontal: 16.0),
-          margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 8.0),
-          child: Column(
-            crossAxisAlignment:
-                isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
-            children: [
-              Text(
-                username,
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: isMe
-                      ? Colors.black
-                      : Theme.of(context).accentTextTheme.headline6.color,
-                ),
-              ),
-              Text(
-                message,
-                style: TextStyle(
-                  fontSize: 16.0,
-                  color: isMe
-                      ? Colors.black
-                      : Theme.of(context).accentTextTheme.headline6.color,
-                ),
-                textAlign: isMe ? TextAlign.end : TextAlign.start,
-              ),
-            ],
-          ),
+          ],
         ),
+        Positioned(
+            top: 3,
+            left: isMe ? null : MediaQuery.of(context).size.width / 1.9,
+            right: isMe ? MediaQuery.of(context).size.width / 1.9 : null,
+            child: CircleAvatar(
+              radius: 25,
+              backgroundImage: NetworkImage(userImage),
+            )),
       ],
+      overflow: Overflow.visible,
     );
   }
 }
